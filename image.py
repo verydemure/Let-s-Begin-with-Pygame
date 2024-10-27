8ba0de219ff5a8012cbb9e6f33d74488205e975d
Radoya aftab <raimaaftab73@gmail.com>
2024-10-27 18:23:11 +0600

    drake

---
+++ image.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+white = (255,255,255)
+
+
+clock = pygame.time.Clock()
+display_surface = pygame.display.set_mode((500,500))
+pygame.display.set_caption('image')
+
+
+DEFAULT_IMAGE_SIZE = (200,200)
+DEFAULT_IMAGE_POSITION = (150,150)
+image = pygame.image.load('noFilter.webp')
+image = pygame.transform.scale(image,DEFAULT_IMAGE_SIZE )
+
+while True:
+
+    display_surface.fill(white)
+    display_surface.blit(image,  DEFAULT_IMAGE_POSITION)
+
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            pygame.quit()   
+            quit
+
+    pygame.display.flip()
+    clock.tick()
